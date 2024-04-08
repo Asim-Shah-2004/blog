d9165a24b3ab6ea6015fef0efdc27b504c9d470d
Asim Shah <asimshah2111@gmail.com>
2024-04-09 02:59:49 +0530

    added formatting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+    const handleNameChange = () => {
+        const names = ["asim", "abcd", "xyz"];
+        const ran = Math.floor(Math.random() * 3);
+        return names[ran];
+    };
 
-  const handleNameChange = () =>{
-    const names = ['asim','abcd','xyz'];
-    const ran = Math.floor(Math.random()*3)
-    return names[ran]
-  }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello {handleNameChange()}</p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Hello {handleNameChange()}</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
